455929c7268b941c4839dad6d6cf6d7e2c12d3f1
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-05-22 14:53:13 +0000

    exercicio01 terminado

---
+++ exercicio01.py
@@ -0,0 +1,9 @@
+curso01 = int(input("Digite a quantidade de acessos do curso01"))
+curso02 = int(input("Digite a quantidade de acessos do curso02"))
+
+if curso01 > curso02 :
+    print("Curso01 tem mais acessos.")
+elif curso02 < curso02 :
+    print("Curso02 tem mais acessos.")
+else:
+    print("Ambos os cursos tiveram a mesma quantidade de acessos.")
